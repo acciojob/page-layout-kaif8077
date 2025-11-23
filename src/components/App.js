7653bd07abffdf531a9ce77cf5591b6d844c550b
kaif8077 <149231751+kaif8077@users.noreply.github.com>
2025-11-23 21:45:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,22 @@ import React from "react";
 
 const App = () => {
 
-  // Simple PageLayout Component
   const PageLayout = ({ header, footer, children }) => {
     return (
       <div>
-        {header}
-        {children}
-        {footer}
+        <div>{header}</div>
+        <div>{children}</div>
+        <div>{footer}</div>
       </div>
     );
   };
 
   return (
     <PageLayout
-      header={<h1>Welcome to my website</h1>}
-      footer={<p>© 2023 My Website. All rights reserved.</p>}
+      header={<h1>Header</h1>}
+      footer={<p>Footer</p>}
     >
-      <p>This is the content of my website</p>
+      <p>This is the content of my website.</p>
     </PageLayout>
   );
 };
