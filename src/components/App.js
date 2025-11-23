53732ad04c6ac071682f85bb71a894a21eae8cf4
Mohammad Kaif <mohdkaif90275@gmail.com>
2025-11-23 21:40:03 +0530

    kaif commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-
 import React from "react";
-import './../styles/App.css';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
 
-export default App
+  // Simple PageLayout Component
+  const PageLayout = ({ header, footer, children }) => {
+    return (
+      <div>
+        {header}
+        {children}
+        {footer}
+      </div>
+    );
+  };
+
+  return (
+    <PageLayout
+      header={<h1>Welcome to my website</h1>}
+      footer={<p>© 2023 My Website. All rights reserved.</p>}
+    >
+      <p>This is the content of my website</p>
+    </PageLayout>
+  );
+};
+
+export default App;
