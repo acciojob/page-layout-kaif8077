ce8463f7e65fb83ce80b5b80a1e86ad62f1f625b
kaif8077 <149231751+kaif8077@users.noreply.github.com>
2025-11-23 21:52:37 +0530

    Update header text in App component

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ const App = () => {
 
   return (
     <PageLayout
-      header={<h1>Header</h1>}  
+      header={<h1>Welcome to my website</h1>}  
       footer={<p>© 2023 My Website. All rights reserved.</p>}  
     >
       <p>This is the content of my website.</p>  
